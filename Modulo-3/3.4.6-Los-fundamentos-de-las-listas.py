0e49c886203a913ffaea7e4ac8a8f8e74683d166
D4vkd <saicoexperiment@gmail.com>
2025-12-05 15:07:55 -0500

    Modulo-3 act 2

---
+++ Modulo-3/3.4.6-Los-fundamentos-de-las-listas.py
@@ -0,0 +1,9 @@
+hat_list = [1, 2, 3, 4, 5]  # Esta es una lista existente de números ocultos en el sombrero.
+
+hat_list [2] = int(input("Ingrese un numero: "))
+
+del hat_list [4] 
+
+print(len(hat_list))
+
+print(hat_list)
